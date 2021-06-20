0a753d10eaf2230b063ad8f6fa36165e9904158f
rustiebeats <skdltm08@gmail.com>
2021-06-20 15:15:27 +0900

    June 20

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 
+const prefix = "!";
+
+client.on("message", function(message){
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if(command === "ping"){
+        const timeTaken = Date.now();
+        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    }
+
+    else if(command === "sum"){
+        const numArgs = args.map(x => parseFloat(x));
+        const sum = numArgs.reduce((counter, x) => counter += x);
+        message.reply(`The sum of all the arguments you provided is ${sum}!`);
+    }
+});
+
 client.login(config.BOT_TOKEN);
